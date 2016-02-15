51269628618bf6e5d5a08549fe02ceabf203a05c
Belousow Makc <lib.bmw@gmail.com>
2016-02-15 15:35:17 +0300

    Add Flag and Register datatypes implementation

--- aioriak/error.py
+++ aioriak/error.py
@@ -17,3 +17,18 @@ class ConflictError(RiakError):
     '''
     def __init__(self, message='Object in conflict'):
         super(ConflictError, self).__init__(message)
+
+
+class ContextRequired(RiakError):
+    '''
+    This exception is raised when removals of map fields and set
+    entries are attempted and the datatype hasn't been initialized
+    with a context.
+    '''
+
+    _default_message = ('A context is required for remove operations, '
+                        'fetch the datatype first')
+
+    def __init__(self, message=None):
+        super(ContextRequired, self).__init__(message or
+                                              self._default_message)
