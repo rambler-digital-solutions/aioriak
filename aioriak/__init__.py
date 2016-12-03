602342824b2d761a8ca8c9606da35b8b647e89aa
pyalex <oleksii.moskalenko@healthjoy.com>
2016-12-03 14:23:52 -0800

    better pep-8 support

--- aioriak/__init__.py
+++ aioriak/__init__.py
@@ -1,5 +1,8 @@
 __version__ = '0.0.1'
 
-from .client import RiakClient  # noqa
-from .riak_object import RiakObject  # noqa
-from .mapreduce import RiakMapReduce  # noqa
+from .client import RiakClient
+from .riak_object import RiakObject
+from .mapreduce import RiakMapReduce
+
+
+__all__ = ('RiakClient', 'RiakObject', 'RiakMapReduce')
