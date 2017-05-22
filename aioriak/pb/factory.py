8508f8ebe3171dd2862d0d712943e86f93056d6c
Belousow Makc <lib.bmw@gmail.com>
2017-05-22 08:51:20 +0300

    Small refactoring

--- aioriak/pb/factory.py
+++ aioriak/pb/factory.py
@@ -1,28 +1,33 @@
 from aioriak.pb import messages
 
 
+RESP_CODES = {
+    messages.MSG_CODE_PING_REQ: messages.MSG_CODE_PING_RESP,
+}
+
+
 class Message:
 
-    def __init__(self, msg_code, data=None, resp_code=None):
-        if msg_code not in messages.MESSAGE_CLASSES:
-            raise ValueError('Unknown MSG_CODE == {}'.format(msg_code))
-        self.msg_code = msg_code
+    def __init__(self, code, data=None, resp_code=None):
+        if code not in messages.MESSAGE_CLASSES:
+            raise ValueError('Unknown MSG_CODE == {}'.format(code))
+        self.code = code
         self.data = data
-        self.resp_code = resp_code
+        self.resp_code = resp_code or RESP_CODES.get(code)
 
         self.encoders = {
             messages.MSG_CODE_PING_REQ: self._encode_ping
         }
 
     def encode(self):
-        encoder = self.encoders.get(self.msg_code)
+        encoder = self.encoders.get(self.code)
         if encoder:
             return encoder()
         else:
-            raise ValueError('Unknown MSG_CODE == {}'.format(self.msg_code))
+            raise ValueError('Unknown MSG_CODE == {}'.format(self.code))
 
     def _encode_ping(self):
         return self.data
 
     def __repr__(self):
-        return 'Message({}, {})'.format(self.msg_code, self.resp_code)
+        return 'Message({}, {})'.format(self.code, self.resp_code)
--- tests/test_message_factory.py
+++ tests/test_message_factory.py
@@ -7,3 +7,5 @@ class TestMessageFactory:
     async def test_ping_encode(self):
         message = Message(messages.MSG_CODE_PING_REQ)
         assert None is message.encode()
+        assert messages.MSG_CODE_PING_REQ == message.code
+        assert messages.MSG_CODE_PING_RESP == message.resp_code
