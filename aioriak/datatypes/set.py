184c7e150a399374e37d5c6f1777a8d6fcea13bd
Pietro Convalle <pietro.convalle@answerdigital.com>
2022-10-05 13:43:21 +0100

    updated for python3.10 compatibility

--- aioriak/datatypes/set.py
+++ aioriak/datatypes/set.py
@@ -3,7 +3,7 @@ from aioriak.datatypes.datatype import Datatype
 from aioriak.datatypes import TYPES
 
 
-class Set(collections.Set, Datatype):
+class Set(collections.abc.Set, Datatype):
     '''A convergent datatype representing a Set with observed-remove
     semantics. Currently strings are the only supported value type.
 
@@ -93,7 +93,7 @@ class Set(collections.Set, Datatype):
         return frozenset(new_value)
 
     def _check_type(self, new_value):
-        if not isinstance(new_value, collections.Iterable):
+        if not isinstance(new_value, collections.abc.Iterable):
             return False
         for element in new_value:
             if not isinstance(element, str):
